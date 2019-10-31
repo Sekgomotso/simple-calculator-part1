586d98ccccc97d9413db03c4e404add977eca305
Sekgomotso_s <motsoshalang@gmail.com>
2019-10-31 16:36:16 +0200

    Simple calculator

--- calculator.js
+++ calculator.js
@@ -3,6 +3,32 @@ function add(a,b) {
 }
 
 module.exports = {
-    add
+    add,
+    sum,
+    multiply,
+    product
+}
+
+function sum() {
+    var sum = 0;
+    
+    for (i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+
+function product() {
+    var product = 1;
+
+    for (i = 1; i < arguments.length; i++) {
+        product *= arguments[i];
+    }
+    return product;
 }
 
